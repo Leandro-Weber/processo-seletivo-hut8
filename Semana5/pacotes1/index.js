85714cadbe0334508295145d3e36cf474535e0ea
unknown <leandrowebertavares@gmail.com>
2022-08-17 15:21:24 -0300

    exercícios semana 5 otterwise

--- Semana5/pacotes1/index.js
+++ Semana5/pacotes1/index.js
@@ -1,4 +1,4 @@
 import { formatDate } from "./formatters.js";
 import multiplica from "./helpers.js";
-//console.log(multiplica(2, 3));
+console.log(multiplica(2, 3));
 console.log(formatDate(new Date("2022, 08, 15")));
